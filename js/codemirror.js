02f79153cdbb5f69037bd9a80e002485bb092cda
Dashstrom <60299112+Dashstrom@users.noreply.github.com>
2021-01-01 19:16:24 +0100

    Merge work and do link between page
    - legend remplacer par figcaption
    - don't forget section and article

--- js/codemirror.js
+++ js/codemirror.js
@@ -1,20 +1,18 @@
-var codes = document.body.getElementsByTagName("code");
-for (var index_code = 0; index_code < codes.length ; index_code++) {
+var codes = document.body.getElementsByTagName("code"); //obtention de toutes les balises code
+for (var index_code = 0; index_code < codes.length ; index_code++) { //pour chaque balise code
     
     var code = codes[index_code];
-    var content = code.innerHTML.trim();
-    code.innerHTML = "";
-    code.style.fontSize = "13px";
+    var content = code.textContent.trim(); //obtention du contenue de la balise
+    code.textContent = ""; //purge du contenue originel pour le remplacer
+    code.style.fontSize = "15px"; //taille de la police
 
     var editor = CodeMirror(code, {
-        value: content,
-        mode: "python",
-        theme: "monokai",
-        tabSize: 4, 
-        lineNumbers: true
+        value: content, //contenue
+        mode: "python", //choix du language
+        theme: "monokai", //theme monokai
+        tabSize: 4, //largeur des tabulation
+        lineNumbers: true //affichage du nombre de ligne
     });
-
-    for(var count=-1, index=-2; index != -1; count++, index=content.indexOf("\n", index+1) );
-    editor.setSize(null, 16.5*count);
-    editor.on('beforeChange',  function(cm, change) { change.cancel(); });
+    editor.setSize(null, 18*editor.lineCount()+10); //mise de l'éditeur à la bonne taille
+    editor.on('beforeChange',  function(cm, change) { change.cancel(); }); // bloque l'écriture
 }
